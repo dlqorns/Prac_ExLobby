46cc50ebe7f723c297235669836128b204d87d28
dlqorns <snack121900@gmail.com>
2024-07-04 21:39:34 +0900

    240704 CodeComments2

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -16,8 +16,8 @@ using Unity.Networking.Transport.Relay;
 
 public class LobbyManager : MonoBehaviour
 {
-    public static LobbyManager instance;
-    const string KEY_RELAY_JOIN_CODE = "RelayJoinCode";
+    public static LobbyManager instance; // instance라는 정적 변수로 LobbyManager 인스턴스 선언.
+    const string KEY_RELAY_JOIN_CODE = "RelayJoinCode"; // 상수 문자열 선언.
 
     Lobby joinedLobby;
 
@@ -27,52 +27,52 @@ public class LobbyManager : MonoBehaviour
     float listLobbiesTimer;
     float listLobbiesTimerMax = 3;
 
-    public event EventHandler<OnLobbyListChangedEventArgs> OnLobbyListChanged;
+    public event EventHandler<OnLobbyListChangedEventArgs> OnLobbyListChanged; // OnLobbyListChangedEventArgs라는 내부 클래스에 OnLobbyListChanged 이벤트 핸들러 선언.
     public class OnLobbyListChangedEventArgs : EventArgs
     {
-        public List<Lobby> lobbyList;
+        public List<Lobby> lobbyList; // List<Lobby> 타입 변수 lobbyList.
     }
     private void Awake()
     {
         instance = this;
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject); // 현재 게임오브젝트 객체가 로드될 때 파괴되지 않도록 설정.
         heartBeatTimer = heartBeatTimerMax;
         listLobbiesTimer = listLobbiesTimerMax;
 
         InitializeUnityAuthentication();
     }
 
-    async void InitializeUnityAuthentication()
+    async void InitializeUnityAuthentication() // '비동기' 메서드 정의.
     {
-        if (UnityServices.State != ServicesInitializationState.Initialized)
+        if (UnityServices.State != ServicesInitializationState.Initialized) // 유니티 서비스가 초기화되지 않았으면.
         {
-            InitializationOptions options = new InitializationOptions();
-            options.SetProfile(UnityEngine.Random.Range(100, 1000).ToString());
-            await UnityServices.InitializeAsync(options);
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            InitializationOptions options = new InitializationOptions(); // InitializationOptions 객체 생성.
+            options.SetProfile(UnityEngine.Random.Range(100, 1000).ToString()); // 임의 프로필 생성.
+            await UnityServices.InitializeAsync(options); // 해당 메서드 호출하여 서비스 초기화.
+            await AuthenticationService.Instance.SignInAnonymouslyAsync(); // 해당 메서드 호출하여 익명으로 로그인.
         }
     }
-    async Task<Allocation> AllocateRelay()
+    async Task<Allocation> AllocateRelay() // '비동기' 메서드 정의.
     {
         try
         {
-            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(NetworkGameManager.MAX_PLAYERS - 1);
-            return allocation;
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(NetworkGameManager.MAX_PLAYERS - 1); // RelayService.Instance.CreateAllocationAsync 메서드 호출하여 할당 생성 후.
+            return allocation; // 반환.
         }
-        catch (RelayServiceException ex)
+        catch (RelayServiceException ex) // 예외 시.
         {
-            Debug.Log(ex);
-            return default;
+            Debug.Log(ex); // 내용 출력하고.
+            return default; // 기본값 반환.
         }
     }
     async Task<string> GetRelayJoinCode(Allocation allocation)
     {
         try
         {
-            string relayCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            return relayCode;
+            string relayCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId); // RelayService.Instance.GetJoinCodeAsync 메서드 호출 후.
+            return relayCode; // 리턴된 조인 코드 반환.
         }
-        catch (RelayServiceException ex)
+        catch (RelayServiceException ex) // 예외 처리.
         {
             Debug.Log(ex);
             return default;
@@ -83,13 +83,13 @@ public class LobbyManager : MonoBehaviour
     {
         try
         {
-            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-            return joinAllocation;
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode); // joinCode 사용하여 RelayService.Instance.JoinAllocationAsync 호출 후.
+            return joinAllocation; // 리턴된 할당 객체 반환.
         }
         catch (RelayServiceException ex)
         {
             Debug.Log(ex);
-            LobbyBrowseUI.instance.LobbyConnectError(ex.Reason.ToString());
+            LobbyBrowseUI.instance.LobbyConnectError(ex.Reason.ToString()); // 이유를 문자열로 반환하여 오류 메시지 표시.
             return default;
 
         }
@@ -98,24 +98,24 @@ public class LobbyManager : MonoBehaviour
     {
         try
         {
-            joinedLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, NetworkGameManager.MAX_PLAYERS, new CreateLobbyOptions
+            joinedLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, NetworkGameManager.MAX_PLAYERS, new CreateLobbyOptions // LobbyService.Instance.CreateLobbyAsync 메서드 호출하여 로비 생성 후 joinedLobby 변수에 할당.
             {
                 IsPrivate = isPrivate
             });
 
-            Allocation allocation = await AllocateRelay();
-            string relayJoinCode = await GetRelayJoinCode(allocation);
+            Allocation allocation = await AllocateRelay(); // AllocateRelay() 함수 호출하여 allocation 객체에 할당.
+            string relayJoinCode = await GetRelayJoinCode(allocation); // allocation을 받는 GetRelayJoinCode 함수 호출하여 리턴된 조인 코드를 relayJoinCode 변수에 저장.
 
-            await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions
+            await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions // LobbyService.Instance.UpdateLobbyAsync 함수 호출하여 로비 정보 업데이트.
             {
-                Data = new Dictionary<string, DataObject>
+                Data = new Dictionary<string, DataObject> 
                 {
-                    { KEY_RELAY_JOIN_CODE, new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) }
+                    { KEY_RELAY_JOIN_CODE, new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) } // 조인 코드를 로비 데이터에 추가.
                 }
             });
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls")); // NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData 호출하여 릴레이 서버 데이터 설정.
 
-            NetworkGameManager.instance.StartHost();
+            NetworkGameManager.instance.StartHost(); // 호스트 시작.
         }
         catch (LobbyServiceException ex)
         {
@@ -127,14 +127,14 @@ public class LobbyManager : MonoBehaviour
     {
         try
         {
-            joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+            joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync(); // LobbyService.Instance.QuickJoinLobbyAsync() 호출하여 joinedLobby 변수에 할당.
 
-            string relayCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
-            JoinAllocation joinAllocation = await JoinRelay(relayCode);
+            string relayCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value; // 로비 데이터에서 조인 코드를 가져와 relayCode 변수에 저장.
+            JoinAllocation joinAllocation = await JoinRelay(relayCode); // JoinRelay 호출하여 relayCode를 joinAllocation에 저장.
 
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls")); // 해당 메서드 호출하여 릴레이 서버 데이터 설정.
 
-            NetworkGameManager.instance.StartClient();
+            NetworkGameManager.instance.StartClient(); // 클라이언트 시작.
         }
         catch (LobbyServiceException ex)
         {
@@ -153,14 +153,14 @@ public class LobbyManager : MonoBehaviour
                 return;
             }
 
-            joinedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            joinedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode); // lobbyCode 받는 LobbyService.Instance.JoinLobbyByCodeAsync 메서드 호출하여 joinedLobby 변수에 할당.
 
-            string relayCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
-            JoinAllocation joinAllocation = await JoinRelay(relayCode);
+            string relayCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value; // 로비 데이터에서 조인 코드 가져와 relayCode에 저장.
+            JoinAllocation joinAllocation = await JoinRelay(relayCode); // JoinRelay 메서드 호출하여 리턴된 할당 객체 저장. 
 
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls")); // 해당 메소드 호출하여 랄레이 서버 데이터 설정.
 
-            NetworkGameManager.instance.StartClient();
+            NetworkGameManager.instance.StartClient(); // 클라이언트 시작.
         }
         catch (LobbyServiceException ex)
         {
@@ -168,6 +168,8 @@ public class LobbyManager : MonoBehaviour
             LobbyBrowseUI.instance.LobbyConnectError(ex.Reason.ToString());
         }
     }
+
+    // 안 씀.
     public async void JoinByID(string lobbyID)
     {
         try
@@ -194,7 +196,7 @@ public class LobbyManager : MonoBehaviour
         {
             try
             {
-                await LobbyService.Instance.RemovePlayerAsync(joinedLobby.Id, AuthenticationService.Instance.PlayerId);
+                await LobbyService.Instance.RemovePlayerAsync(joinedLobby.Id, AuthenticationService.Instance.PlayerId); // 해당 메서드 호출하여 플레이어 로비에서 제거.
                 joinedLobby = null;
             }
             catch (LobbyServiceException ex)
@@ -209,7 +211,7 @@ public class LobbyManager : MonoBehaviour
         {
             try
             {
-                await LobbyService.Instance.DeleteLobbyAsync(joinedLobby.Id);
+                await LobbyService.Instance.DeleteLobbyAsync(joinedLobby.Id); // 해당 메소드 호출하여 로비 삭제.
                 joinedLobby = null;
             }
             catch (LobbyServiceException ex)
@@ -227,18 +229,18 @@ public class LobbyManager : MonoBehaviour
             {
                 Filters = new List<QueryFilter>
                 {
-                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT) // 사용 가능 슬롯이 0보다 큰 로비를 필터링하도록 설정.
                 },
 
                 Order = new List<QueryOrder>()
                 {
-                    new QueryOrder(asc: false, field: QueryOrder.FieldOptions.Created)
+                    new QueryOrder(asc: false, field: QueryOrder.FieldOptions.Created) // 로비 생성 날짜 기준 내림차순으로 정렬하도록 설정.
                 }
             };
-            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(options);
-            OnLobbyListChanged?.Invoke(this, new OnLobbyListChangedEventArgs
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(options); // 해당 메서드 호출하여 로비 목록 쿼리하고, 결과를 queryResponse에 저장.
+            OnLobbyListChanged?.Invoke(this, new OnLobbyListChangedEventArgs // OnLobbyListChanged 이벤트 발생시킨 후.
             {
-                lobbyList = queryResponse.Results
+                lobbyList = queryResponse.Results // 쿼리 결과를 lobbyList에 인자로 전달.
             });
         }
         catch (LobbyServiceException ex)
@@ -248,43 +250,43 @@ public class LobbyManager : MonoBehaviour
 
     }
 
-    private void Update()
+    private void Update() // 매 프레임 호출.
     {
         LobbyHeartBeat();
         LobbyListUpdate();
     }
     void LobbyHeartBeat()
     {
-        if (IsLobbyHost())
+        if (IsLobbyHost()) // 현재 플레이어가 로비 호스트면
         {
-            heartBeatTimer -= Time.deltaTime;
-            if (heartBeatTimer <= 0)
+            heartBeatTimer -= Time.deltaTime; // heartBeatTimer를 감소시키고.
+            if (heartBeatTimer <= 0) // 0 이하가 되면.
             {
-                heartBeatTimer = heartBeatTimerMax;
-                LobbyService.Instance.SendHeartbeatPingAsync(joinedLobby.Id);
+                heartBeatTimer = heartBeatTimerMax; // 초기화 후.
+                LobbyService.Instance.SendHeartbeatPingAsync(joinedLobby.Id); // 해당 메서드를 호출하여 Heartbeat를 전송.
             }
         }
     }
     void LobbyListUpdate()
     {
-        if (joinedLobby == null && AuthenticationService.Instance.IsSignedIn &&
-            SceneManager.GetSceneByName("MULTIPLAYER_LOBBY_BROWSE_SCENE") == SceneManager.GetActiveScene())
+        if (joinedLobby == null && AuthenticationService.Instance.IsSignedIn && // 로비에 참가하지 않았고 인증 서비스에 로그인돼 있으며.
+            SceneManager.GetSceneByName("MULTIPLAYER_LOBBY_BROWSE_SCENE") == SceneManager.GetActiveScene()) // 현재 MULTIPLAYER_LOBBY_BROWSE_SCENE 씬인지 확인.
         {
-            listLobbiesTimer -= Time.deltaTime;
-            if (listLobbiesTimer <= 0)
+            listLobbiesTimer -= Time.deltaTime; // listLobbiesTimer를 감소시키고.
+            if (listLobbiesTimer <= 0) // 0 이하가 되면.
             {
-                listLobbiesTimer = listLobbiesTimerMax;
-                ListLobbies();
+                listLobbiesTimer = listLobbiesTimerMax; // 초기화 후.
+                ListLobbies(); // 해당 메서드 호출하여 로비 목록 업데이트.
             }
         }
     }
     bool IsLobbyHost()
     {
-        return joinedLobby != null && joinedLobby.HostId == AuthenticationService.Instance.PlayerId;
+        return joinedLobby != null && joinedLobby.HostId == AuthenticationService.Instance.PlayerId; // 로비에 참가하지 않았고 호스트 아이디와 플레이어 아이디가 동일한지 bool 값 반환.
     }
 
     public Lobby GetJoinedLobby()
     {
-        return joinedLobby;
+        return joinedLobby; // 현재 참가한 로비 객체 반환.
     }
 }
--- Assets/Scripts/NetworkGameManager.cs
+++ Assets/Scripts/NetworkGameManager.cs
@@ -9,40 +9,41 @@ public class NetworkGameManager : NetworkBehaviour
     public const int MAX_PLAYERS = 12;
     public GameObject playerPrefab;
 
-    public NetworkList<PlayerData> playerDataNetworkList;
-    public delegate void OnPlayerDataListChanged();
-    public static OnPlayerDataListChanged onPlayerDataListChanged;
+    public NetworkList<PlayerData> playerDataNetworkList; // 네트워크 상에서 플레이어 데이터를 관리하는 List.
+    public delegate void OnPlayerDataListChanged(); // 플레이어 데이터 리스트 변경 시 호출될 델리게이트 정의.
+    public static OnPlayerDataListChanged onPlayerDataListChanged; // 델리게이트 인스턴스.
 
-    public GameObject myPlayer; // only set when ingame;
+    public GameObject myPlayer; // only set when ingame, 로컬 플레이어 오브젝트 저장 변수. 
 
     string username;
-    public enum GameState // only allow players to join while waiting to start
+    public enum GameState // only allow players to join while waiting to start, 게임 상태 나타내는 열거형 정의.
     {
-        WaitingToStart,
-        InHub,
-        InGame,
-        End
+        WaitingToStart, // 게임 시작 기다리는 상태.
+        InHub, // 로비에 있는 상태.
+        InGame, // 게임 중.
+        End // 게임 끝.
     }
-    public NetworkVariable<GameState> gameState = new NetworkVariable<GameState>(GameState.WaitingToStart);
+    public NetworkVariable<GameState> gameState = new NetworkVariable<GameState>(GameState.WaitingToStart); // 네트워크에서 정의되는 게임 상태 변수 정의, 초기값은 WaitingToStart 상태로.
     private void Awake()
     {
-        if (NetworkGameManager.instance == null)
+        // 싱글톤 패턴 구현.
+        if (NetworkGameManager.instance == null) // instance가 비어있으면
         {
-            instance = this;
+            instance = this; // 현재 인스턴스로 설정.
         }
-        else
+        else // 그렇지 않으면.
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // 자신을 파괴.
         }
         playerDataNetworkList = new NetworkList<PlayerData>();
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject); // 이 게임 오브젝트가 씬 전환 시 파괴되지 않도록 함.
 
-        username = PlayerPrefs.GetString("USERNAME", "Guest: " + Random.Range(100, 1000));
+        username = PlayerPrefs.GetString("USERNAME", "Guest: " + Random.Range(100, 1000)); // PlayerPrefs에 저장된 사용자 이름 불러오고 없으면 무작위.
     }
 
     private void Start()
     {
-        playerDataNetworkList.OnListChanged += PlayerDataNetworkList_OnListChanged;
+        playerDataNetworkList.OnListChanged += PlayerDataNetworkList_OnListChanged; // playerDataNetworkList가 변경될 때 호출되는 이벤트 핸들러 등록.
     }
 
     public string GetUsername()
@@ -51,54 +52,56 @@ public class NetworkGameManager : NetworkBehaviour
     }
     public void SetUsername(string _username)
     {
-        if (string.IsNullOrWhiteSpace(_username))
+        if (string.IsNullOrWhiteSpace(_username)) // 이름이 비어있거나 공백인 경우.
         {
-            username = "Guest: " + Random.Range(100, 1000);
+            username = "Guest: " + Random.Range(100, 1000); // 무작위.
         }
         else
         {
              username = _username;
         }
 
-        PlayerPrefs.SetString("USERNAME", username);
+        PlayerPrefs.SetString("USERNAME", username); // PlayerPrefs에 설정된 이름 저장.
     }
 
     public string GetUsernameFromClientId(ulong _clientId)
     {
-        foreach (PlayerData playerData in playerDataNetworkList)
+        foreach (PlayerData playerData in playerDataNetworkList) // playerDataNetworkList의 playerData 순회.
         {
-            if (playerData.clientId == _clientId)
-                return playerData.username.ToString();
+            if (playerData.clientId == _clientId) // 둘이 같으면.
+                return playerData.username.ToString(); // 문자열로 유저네임 출력.
         }
         return default;
     }
-    private void PlayerDataNetworkList_OnListChanged(NetworkListEvent<PlayerData> changeEvent)
+    private void PlayerDataNetworkList_OnListChanged(NetworkListEvent<PlayerData> changeEvent) // 이벤트 핸들러.
     {
         //Debug.Log("Invoke");
         //Debug.Log(playerDataNetworkList.Count);
-        onPlayerDataListChanged?.Invoke();
+        onPlayerDataListChanged?.Invoke(); // 등록된 델리게이트 호출.
     }
-    public bool IsPlayerIndexConnected(int playerIndex)
+    public bool IsPlayerIndexConnected(int playerIndex) // 주어진 인덱스의 플레이어가 연결되어 있는지.
     {
         return playerIndex < playerDataNetworkList.Count;
     }
-    public PlayerData GetPlayerDataFromIndex(int _playerIndex)
+    public PlayerData GetPlayerDataFromIndex(int _playerIndex) // 인덱스를 통해.
     {
-        return playerDataNetworkList[_playerIndex];
+        return playerDataNetworkList[_playerIndex]; // 플레이어 데이터 반환.
     }
     public PlayerData GetPlayerDataFromClientId(ulong clientId)
     {
-        foreach (PlayerData playerData in playerDataNetworkList)
+        foreach (PlayerData playerData in playerDataNetworkList) // playerDataNetworkList에서 playerData 순회.
         {
-            if (playerData.clientId == clientId)
-                return playerData;
+            if (playerData.clientId == clientId) // 둘이 같으면.
+                return playerData; // playerData 반환.
         }
         return default;
     }
     public PlayerData GetLocalPlayerData()
     {
-        return GetPlayerDataFromClientId(NetworkManager.Singleton.LocalClientId);
+        return GetPlayerDataFromClientId(NetworkManager.Singleton.LocalClientId); // 로컬 클라이언트 플레이어 데이터 반환.
     }
+
+    // 클라이언트 ID를 통해 플레이어 데이터의 인덱스를 반환하는 메서드.
     public int GetPlayerDataIndexFromClientID(ulong clientId)
     {
         for (int i = 0; i < playerDataNetworkList.Count; i++)
@@ -108,16 +111,20 @@ public class NetworkGameManager : NetworkBehaviour
         }
         return -1;
     }
+
+    // 안 씀.
     public int GetPlayerSkinFromIndex(int _playerIndex)
     {
         return playerDataNetworkList[_playerIndex].skinIndex;
     }
 
+    // 안 씀.
     public void ChangePlayerSkin(int skinIndex)
     {
         ChangePlayerSkinServerRpc(skinIndex);
     }
 
+    // 안 씀.
     [ServerRpc(RequireOwnership = false)]
     void ChangePlayerSkinServerRpc(int skinIndex, ServerRpcParams rpcParams = default)
     {
@@ -127,24 +134,25 @@ public class NetworkGameManager : NetworkBehaviour
         playerDataNetworkList[playerIndex] = data;
     }
 
+    // 호스트 시작 메서드.
     public void StartHost()
     {
-        NetworkManager.Singleton.ConnectionApprovalCallback += Network_ConnectionApprovalCallback;
-        NetworkManager.Singleton.OnClientConnectedCallback += Network_Server_OnClientConnectedCallback;
-        NetworkManager.Singleton.OnClientDisconnectCallback += Network_Server_OnClientDisconnectCallback;
+        NetworkManager.Singleton.ConnectionApprovalCallback += Network_ConnectionApprovalCallback; // 연결 승인 콜백 등록.
+        NetworkManager.Singleton.OnClientConnectedCallback += Network_Server_OnClientConnectedCallback; // 클라이언트 연결 콜백 등록.
+        NetworkManager.Singleton.OnClientDisconnectCallback += Network_Server_OnClientDisconnectCallback; // 클라이언트 연결 해제 콜백 등록.
 
-        NetworkManager.Singleton.StartHost();
-        LoadLobbyJoinedScene();
+        NetworkManager.Singleton.StartHost(); // 호스트 시작.
+        LoadLobbyJoinedScene(); // 로비 씬 로드.
     }
 
-    private void Network_Server_OnClientDisconnectCallback(ulong _clientId)
+    private void Network_Server_OnClientDisconnectCallback(ulong _clientId) // 클라이언트가 연결 끊었을 때 호출되는 콜백.
     {
         for (int i = 0; i < playerDataNetworkList.Count; i++)
         {
             PlayerData data = playerDataNetworkList[i];
             if (data.clientId == _clientId)
             {
-                playerDataNetworkList.RemoveAt(i);
+                playerDataNetworkList.RemoveAt(i); // 연결 끊긴 클라이언트의 플레이어 데이터를 리스트에서 제거.
             }
         }
 
@@ -153,41 +161,43 @@ public class NetworkGameManager : NetworkBehaviour
             //Scoreboard.Instance.ResetScoreboard();
         }
     }
-    private void Network_Server_OnClientConnectedCallback(ulong _clientId)
+    private void Network_Server_OnClientConnectedCallback(ulong _clientId) // 클라이언트가 연결되었을 때 호출되는 콜백.
     {
-        playerDataNetworkList.Add(new PlayerData
+        playerDataNetworkList.Add(new PlayerData // 새 플레이어 데이터를 리스트에 추가.
         {
             clientId = _clientId,
-            skinIndex = 0
+            skinIndex = 0 // 안 씀.
         });
-        SetUsernameServerRpc(GetUsername());
+        SetUsernameServerRpc(GetUsername()); // 사용자 이름 설정을 위한 ServerRpc 호출.
     }
-    public void StartClient()
+    public void StartClient() // 클라이언트 시작.
     {
-        NetworkManager.Singleton.OnClientDisconnectCallback += Network_OnClientDisconnectCallback;
-        NetworkManager.Singleton.OnClientConnectedCallback += Network_Client_OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += Network_OnClientDisconnectCallback; // 클라이언트 연결 해제 콜백 등록.
+        NetworkManager.Singleton.OnClientConnectedCallback += Network_Client_OnClientConnectedCallback; // 클라이언트 연결 콜백 등록.
 
-        NetworkManager.Singleton.StartClient();
+        NetworkManager.Singleton.StartClient(); // 클라이언트 시작.
     }
 
-    private void Network_Client_OnClientConnectedCallback(ulong obj)
+    private void Network_Client_OnClientConnectedCallback(ulong obj) // 클라이언트가 연결되었을 때 호출되는 콜백.
     {
-        SetUsernameServerRpc(GetUsername());
+        SetUsernameServerRpc(GetUsername()); // 사용자 이름 설정을 위한 ServerRpc 호출.
         if (SceneManager.GetActiveScene().name == "MULTIPLAYER_GAME_SCENE")
         {
             //Scoreboard.Instance.ResetScoreboard();
         }
     }
+
+    //  // 사용자 이름 설정을 위한 ServerRpc.
     [ServerRpc(RequireOwnership = false)]
     void SetUsernameServerRpc(string _username, ServerRpcParams rpcParams = default)
     {
-        int playerIndex = GetPlayerDataIndexFromClientID(rpcParams.Receive.SenderClientId);
-        PlayerData data = playerDataNetworkList[playerIndex];
-        data.username = _username;
-        playerDataNetworkList[playerIndex] = data;
+        int playerIndex = GetPlayerDataIndexFromClientID(rpcParams.Receive.SenderClientId); // 클라이언트 ID를 찾아 playerIndex에 넣고.
+        PlayerData data = playerDataNetworkList[playerIndex]; // 반환된 값을 playerDataNetworkList에서 찾아 data에 할당.
+        data.username = _username; // 사용자 이름 설정.
+        playerDataNetworkList[playerIndex] = data; // 바뀐 data를 다시 playerDataNetworkList에 할당.
     }
 
-    private void Network_OnClientDisconnectCallback(ulong clientId)
+    private void Network_OnClientDisconnectCallback(ulong clientId) // 클라이언트가 연결을 끊었을 때 호출되는 콜백.
     {
         //Debug.Log("2");
         if (SceneManager.GetSceneByName("MULTIPLAYER_LOBBY_BROWSE_SCENE") == SceneManager.GetActiveScene())
@@ -209,6 +219,7 @@ public class NetworkGameManager : NetworkBehaviour
         //throw new System.NotImplementedException();
     }
 
+    // 연결 승인 콜백.
     void Network_ConnectionApprovalCallback(NetworkManager.ConnectionApprovalRequest connectionApprovalRequest, NetworkManager.ConnectionApprovalResponse connectionApprovalResponse)
     {
         //Debug.Log("1");
@@ -224,7 +235,7 @@ public class NetworkGameManager : NetworkBehaviour
             connectionApprovalResponse.Reason = "Game is full.";
             return;
         }
-        connectionApprovalResponse.Approved = true;
+        connectionApprovalResponse.Approved = true; // 위 조건 전부 피할 시 연결 승인.
         //connectionApprovalResponse.CreatePlayerObject = true; 
     }
 
@@ -241,12 +252,13 @@ public class NetworkGameManager : NetworkBehaviour
         NetworkManager.Singleton.SceneManager.LoadScene("MULTIPLAYER_GAME_SCENE", LoadSceneMode.Single);
     }
 
+    // 서버에서 연결된 클라이언트에 대해 플레이어 오브젝트를 생성하고 스폰하는 메서드.
     public void SpawnPlayers() // server
     {
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds) // NetworkManager.Singleton.ConnectedClientsIds의 clientId 순회.
         {
-            GameObject player = Instantiate(playerPrefab);
-            player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
+            GameObject player = Instantiate(playerPrefab); // player 오브젝트에 플레이어 프리팹을 인스턴스화하고.
+            player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true); // 연결된 각 클라이언트 ID에 대해 인스턴스한 프리팹을 NetworkObject를 사용하여 스폰.
         }
     }
 
